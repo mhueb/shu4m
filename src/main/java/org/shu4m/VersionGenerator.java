e87e6e41dc70295906da8accae3278084aaedca2
mhueb <mhueb@pobox.com>
2013-12-15 19:41:04 +0100

    misc

--- src/main/java/org/shu4m/VersionGenerator.java
+++ src/main/java/org/shu4m/VersionGenerator.java
@@ -1,3 +1,18 @@
+/* 
+ * Copyright 2013 Matthias Huebner
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.shu4m;
 
 import java.io.File;
@@ -16,6 +31,12 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
+/**
+ * Generate Version class file from pom.xml
+ * 
+ * @author mhueb
+ * 
+ */
 @Mojo(name = "generate-version", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class VersionGenerator extends AbstractMojo {
 
